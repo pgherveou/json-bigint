98636422dbe7931d7e3196c62852450f6de551a1
Pierre Guillaume Herveou <pgherveou@lyft.com>
2020-05-19 09:04:20 -0400

    rework exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-// /Users/pgherveou/Github/pgherveou/json-bigint/node_modules/bignumber.js/bignumber.d.ts
-
-import { stringify } from "./stringify";
-import { parse } from "./parse";
-
-const JSON = {
-  stringify,
-  parse,
-};
-
-export default JSON;
+export { stringify, noQuote } from "./stringify";
+export { parse } from "./parse";
